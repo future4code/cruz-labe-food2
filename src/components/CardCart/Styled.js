7c6f414e118f4af33641d062d23d023518ee1e26
andremuniz92 <andre_luis_92@hotmail.com>
2021-05-03 15:52:42 -0300

    useProtectedPage e começo de logica do carrinho

---
+++ src/components/CardCart/Styled.js
@@ -0,0 +1,15 @@
+import styled from 'styled-components'
+
+export const Title = styled.p`
+    width: 5.688rem;
+    height: 1.188rem;
+    font-family: Roboto;
+    font-size: 1rem;
+    font-weight: normal;
+    font-stretch: normal;
+    font-style: normal;
+    line-height: normal;
+    letter-spacing: -0.39px;
+    text-align: center;
+    color: var(--black);
+` 
