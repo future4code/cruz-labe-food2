4cf9b4a3bb96661672ac39018db72dd5bbe06148
Milena-Ribeiro <milenai@academico.faculdadeages.edu.br>
2021-05-03 15:48:46 -0300

    criando input

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import './App.css';
+import Input from './components/Input/Input'
 
 function App() {
   return (
     <div className="App">
-
+      <Input label="Nome" placeholder="Nome e sobrenome" />
     </div>
   );
 }
