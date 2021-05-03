21142025b81e5af8f7749c95b80e5cbbb4d7d5e3
Igor Meira do Nascimento Silva <igormnsilva@gmail.com>
2021-05-03 14:59:07 -0300

    rotas e coordenadas criadas

---
+++ src/Routes/Coordinators.js
@@ -0,0 +1,21 @@
+export const goToLogin = (history) =>{
+    history.push('/login')
+}
+export const goToSignUp = (history) =>{
+    history.push('/signup')
+}
+export const goToHome = (history) =>{
+    history.push('/')
+}
+export const goToProfile = (history) =>{
+    history.push('/profile')
+}
+export const goToCart = (history) =>{
+    history.push('/cart')
+}
+export const goToEditProfile = (history) =>{
+    history.push('/edit-profile')
+}
+export const goToEditAddress = (history) =>{
+    history.push('/edit-address')
+}
