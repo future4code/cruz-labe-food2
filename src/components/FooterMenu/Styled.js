8e957aa3945feae5fb55f07f4f4ed8595faf26fe
andremuniz92 <andre_luis_92@hotmail.com>
2021-05-04 16:22:43 -0300

    tela carrinho com footer

--- src/components/FooterMenu/Styled.js
+++ src/components/FooterMenu/Styled.js
@@ -4,25 +4,30 @@ export const MainFooter = styled.div`
     display: flex;
     flex-flow: row nowrap;
     justify-content: center;
-    position: absolute;
     position: fixed;
-    bottom: 20px;
-    height: 100px;
-    left: 0;
-    right: 0;
+    bottom: 1px;
+    border-top: 1px solid black;
 `
-export const HomeButton = styled.button`
+export const HomeButton = styled.div`
     width: 7.5rem;
     height: 3.062rem;
     padding: 0.688rem 2.875rem 0.688rem 2.938rem;
 `
-export const CartButton = styled.button`
+export const CartButton = styled.div`
     width: 7.5rem;
     height: 3.062rem;
     padding: 0.688rem 2.875rem 0.688rem 2.938rem;
 `
-export const ProfileButton = styled.button`
+export const ProfileButton = styled.div`
     width: 7.5rem;
     height: 3.062rem;
     padding: 0.688rem 2.875rem 0.688rem 2.938rem;
 `
+
+export const IconImage = styled.img`
+    width: 1.688rem;
+    height: 1.688rem;
+    object-fit: contain;
+    fill: red;
+` 
+  
--- src/screens/CartPage.js
+++ src/screens/CartPage.js
@@ -1,28 +1,43 @@
 import React, {useContext} from 'react'
 import GlobalStateContext from '../global/GlobalStateContext'
 import CardCart from '../components/CardCart/CardCart'
+import FooterMenu from '../components/FooterMenu/FooterMenu'
+import axios from 'axios'
+import { BASE_URL } from '../constants/urls'
 
 
 const CartPage = () => {
     const { cart, setCart } = useContext(GlobalStateContext)
     const products = [] // inserir estado global dos produtos 
 
-    const addItemToCart = (newItem) => {
-        const index = cart.findIndex((i) => i.id === newItem.id);
-        let newCart = [...cart]
-        if (index === -1) {
-            newCart.push({ ...newItem, amount: 1})
-        } else {
-            newCart[index].amount += 1
-        }
-        setCart(newCart)
-        alert('Prato adicionado ao carrinho!')
-    }
 
+    const placeOrder = () => {
+        const body = {
+            "products": [{
+                "id": "CnKdjU6CyKakQDGHzNln",
+                "quantity": 10
+            }, {
+                "quantity": 1,
+                "id": "KJqMl2DxeShkSBevKVre"
+            }],
+            "paymentMethod": "creditcard"
+        }
+        axios.get(`${BASE_URL}restaurants/1/order`, body, {
+        headers: {
+            auth: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6Im95ZHhmT3IxSlFWbmpJZTBxMEMyIiwibmFtZSI6IkFuZHJlIiwiZW1haWwiOiJhbmRyZUBmdXR1cmU0LmNvbSIsImNwZiI6IjIyMi4yMjIuMjIyLTU1IiwiaGFzQWRkcmVzcyI6dHJ1ZSwiYWRkcmVzcyI6IlIuIEFmb25zbyBCcmF6LCAxNzcsIDcxIC0gVmlsYSBOLiBDb25jZWnDp8OjbyIsImlhdCI6MTYyMDE1NDM3Mn0.ncNcA4RIwKkbWfAbr1cKD9HBxDkmClOqClOhspbzzmM'
+        }
+    })
+    .then((res)=> {
+        console.log(res)
+    }).catch((err)=>{
+        console.log(err)
+    })
+    }
 
     return (
             <div>
-                <CardCart/>
+                <CardCart onClick={placeOrder}/>
+                
             </div>
             )
 
