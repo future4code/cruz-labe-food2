7af8d4944bfe382706d3affe914a53fcef27e5e3
kelvia sousa <kelviaks@gmail.com>
2021-05-05 16:25:03 -0300

    animação ao passar a tela inicial

--- src/components/InitialAnimation/Styled.js
+++ src/components/InitialAnimation/Styled.js
@@ -1,10 +1,10 @@
-import styled from 'styled-components';
+import styled, {keyframes} from 'styled-components';
 
 export const InitialContainer = styled.div`
   display: flex;
   align-items: center;
   justify-content: center;
-  background-color: #e8222e;
+  background-color: #E8222E;
   max-width: 100%;
   width: 500px;
   height: 100vh;
@@ -12,4 +12,19 @@ export const InitialContainer = styled.div`
     width: 7.875rem;
     height: 4.063rem;
   }
+`
+const fadeIn = keyframes`
+  100%{
+    transform:translate(-100%);
+  }
+`
+export const Container= styled.div`
+      overflow: hidden;
+     .show{
+     -webkit-animation: 0.5s ${fadeIn} ease-out;
+     -webkit-animation-fill-mode: forwards;
+    }
+    .notShow{
+     
+    }
 `
