77a36c629643f07678ce5a34950b7a5c7ba9e7cd
Milena-Ribeiro <milenai@academico.faculdadeages.edu.br>
2021-05-06 11:07:23 -0300

    criando funcionalidade de busca

--- src/components/MenuHeader/MenuHeader.js
+++ src/components/MenuHeader/MenuHeader.js
@@ -3,12 +3,12 @@ import { Header, ContainerIcon } from './Styled'
 import {ReactComponent as BackIcon} from '../../assets/back.svg'
 import { useHistory } from 'react-router-dom'
 
-export default function MenuHeader({currentPageLabel, isGoBack}) {
+export default function MenuHeader({currentPageLabel, isGoBack, goTo}) {
     const history = useHistory()
 
     return (
         <Header>
-            {isGoBack && <ContainerIcon onClick={history.goBack}>
+            {isGoBack && <ContainerIcon onClick={goTo ? goTo : history.goBack}>
                 <BackIcon />
             </ContainerIcon>}
             {currentPageLabel}
